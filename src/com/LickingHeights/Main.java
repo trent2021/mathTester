1bf6b94bd69baafe5606e3cdf26bb1f43ab17edd
Trent <trent.polenchar.2021@student.lhschools.org>
2019-01-29 13:39:24 -0500

    Added firstWord+secondWord firstWord+firstNumber

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,9 +3,17 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
+        String firstWord;
+        String secondWord;
         int firstNumber, secondNumber;
 
+        firstWord = "dog";
+        secondWord = "house";
         firstNumber = 5;
         secondNumber = 10;
+
+        System.out.println(firstWord+secondWord);
+        System.out.println(firstWord+firstNumber);
+
     }
 }
