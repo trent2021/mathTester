836a469653f2fbbeb674a8a125b1aeff48cf052b
Trent <trent.polenchar.2021@student.lhschools.org>
2019-01-29 13:45:40 -0500

    Added math equations

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -13,7 +13,12 @@ public class Main {
         secondNumber = 10;
 
         System.out.println(firstWord+secondWord);
-        System.out.println(firstWord+firstNumber);
+        System.out.println(firstWord+(firstNumber+secondNumber));
+        System.out.println("firstNumber + secondNumber is " + (firstNumber+secondNumber));
+        System.out.println("firstNumber - secondNumber is " + (firstNumber-secondNumber));
+        System.out.println("secondNumber / firstNumber is " + (secondNumber/firstNumber));
+        System.out.println("firstNumber * secondNumber is " + (firstNumber*secondNumber));
+        
 
     }
 }
