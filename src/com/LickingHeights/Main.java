27edd2d49e81c79b743d9f1971dd869a809dcd95
Trent <trent.polenchar.2021@student.lhschools.org>
2019-02-12 13:46:42 -0500

    Added statements for conditional operations

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,23 +3,17 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-        String firstWord;
-        String secondWord;
-        int firstNumber, secondNumber;
-
-        firstWord = "dog";
-        secondWord = "house";
-        firstNumber = 5;
-        secondNumber = 10;
-
-        System.out.println(firstWord+secondWord);
-        System.out.println(firstWord+(firstNumber+secondNumber));
-        System.out.println("firstNumber + secondNumber is " + (firstNumber+secondNumber));
-        System.out.println("firstNumber - secondNumber is " + (firstNumber-secondNumber));
-        System.out.println("secondNumber / firstNumber is " + (secondNumber/firstNumber));
-        System.out.println("firstNumber * secondNumber is " + (firstNumber*secondNumber));
-        System.out.println("The modulus of the secondNumber by the firstNumber" +
-                " is "+ (secondNumber%firstNumber));
+        int firstNumber = 10;
+        int secondNumber = 20;
+         if (firstNumber>secondNumber){
+            System.out.println(firstNumber +" is less");
+        }
+        else if(firstNumber<0) {
+            System.out.println(firstNumber + " is greater than 0");
+        }
+        else{
+            System.out.println("I don't know what you want");
+        }
 
     }
 }
